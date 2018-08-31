81b1557fcdea7fb872dc19294d82920c67bb571c
li.zhao <li.zhao@yanwei365.com>
2018-08-31 17:17:59 +0800

    修正antd的Form组件测试

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,32 +1,34 @@
 import React, { Component } from "react";
 // import ReactDom from 'react-dom';
-import Axios from "axios";
+// import Axios from "axios";
 import { Form, Icon, Input, Button, Checkbox } from "antd";
-
+import "../../assets/css/test.css";
 const FormItem = Form.Item;
 
-class FormContent extends Component {
-  constructor(props) {
-    super(props);
-    this.submit.bind(this);
-    this.state = {
-      form: {
-        userName: "",
-        password: ""
+class NormalLoginForm extends Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.submit.bind(this);
+  //   this.state = {
+  //     form: {
+  //       userName: "",
+  //       password: ""
+  //     }
+  //   };
+  // }
+  submit = e => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log("Received values of form: ", values);
+        this.props.onSub(values);
       }
-    };
-  }
-  submit(e) {
-    // this.props.form.validateFields((err, values) => {
-    //     if (!err) {
-    console.log("Received values of form: ", this);
-    //     }
-    //   });
-  }
+    });
+  };
   render() {
     const { getFieldDecorator } = this.props.form;
     return (
-        <Form>
+      <Form onSubmit={this.submit}>
         <FormItem
           label="用户名"
           labelCol={{ span: 3 }}
@@ -38,6 +40,7 @@ class FormContent extends Component {
             <Input
               prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />}
               placeholder="Username"
+              // autoComplete="off"
             />
           )}
         </FormItem>
@@ -53,7 +56,7 @@ class FormContent extends Component {
           )}
         </FormItem>
         <FormItem>
-          <Button type="primary" onClick={this.submit}>
+          <Button type="primary" htmlType="submit">
             提交
           </Button>
         </FormItem>
@@ -61,5 +64,53 @@ class FormContent extends Component {
     );
   }
 }
-const MyForm = Form.create()(FormContent);
+
+// class NormalLoginForm extends React.Component {
+//   handleSubmit = (e) => {
+//     e.preventDefault();
+//     this.props.form.validateFields((err, values) => {
+//       if (!err) {
+//         console.log('Received values of form: ', values);
+//       }
+//     });
+//   }
+
+//   render() {
+//     const { getFieldDecorator } = this.props.form;
+//     return (
+//       <Form onSubmit={this.handleSubmit} className="login-form">
+//         <FormItem>
+//           {getFieldDecorator('userName', {
+//             rules: [{ required: true, message: 'Please input your username!' }],
+//           })(
+//             <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
+//           )}
+//         </FormItem>
+//         <FormItem>
+//           {getFieldDecorator('password', {
+//             rules: [{ required: true, message: 'Please input your Password!' }],
+//           })(
+//             <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+//           )}
+//         </FormItem>
+//         <FormItem>
+//           {getFieldDecorator('remember', {
+//             valuePropName: 'checked',
+//             initialValue: true,
+//           })(
+//             <Checkbox>Remember me</Checkbox>
+//           )}
+//           <a className="login-form-forgot" href="">Forgot password</a>
+//           <Button type="primary" htmlType="submit" className="login-form-button">
+//             Log in
+//           </Button>
+//           Or <a href="">register now!</a>
+//         </FormItem>
+//       </Form>
+//     );
+//   }
+// }
+
+const MyForm = Form.create()(NormalLoginForm);
+// const MyForm = Form.create()(FormContent);
 export default MyForm;
