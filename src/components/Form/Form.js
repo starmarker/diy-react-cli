5815c436c13faa3a666e9b218a0b7e63f22618d1
‘starmark’ <starmark@qq.com>
2018-08-31 20:40:38 +0800

    linux下部分模块更新

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -6,16 +6,6 @@ import "../../assets/css/test.css";
 const FormItem = Form.Item;
 
 class NormalLoginForm extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.submit.bind(this);
-  //   this.state = {
-  //     form: {
-  //       userName: "",
-  //       password: ""
-  //     }
-  //   };
-  // }
   submit = e => {
     e.preventDefault();
     this.props.form.validateFields((err, values) => {
@@ -64,52 +54,6 @@ class NormalLoginForm extends Component {
     );
   }
 }
-
-// class NormalLoginForm extends React.Component {
-//   handleSubmit = (e) => {
-//     e.preventDefault();
-//     this.props.form.validateFields((err, values) => {
-//       if (!err) {
-//         console.log('Received values of form: ', values);
-//       }
-//     });
-//   }
-
-//   render() {
-//     const { getFieldDecorator } = this.props.form;
-//     return (
-//       <Form onSubmit={this.handleSubmit} className="login-form">
-//         <FormItem>
-//           {getFieldDecorator('userName', {
-//             rules: [{ required: true, message: 'Please input your username!' }],
-//           })(
-//             <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
-//           )}
-//         </FormItem>
-//         <FormItem>
-//           {getFieldDecorator('password', {
-//             rules: [{ required: true, message: 'Please input your Password!' }],
-//           })(
-//             <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
-//           )}
-//         </FormItem>
-//         <FormItem>
-//           {getFieldDecorator('remember', {
-//             valuePropName: 'checked',
-//             initialValue: true,
-//           })(
-//             <Checkbox>Remember me</Checkbox>
-//           )}
-//           <a className="login-form-forgot" href="">Forgot password</a>
-//           <Button type="primary" htmlType="submit" className="login-form-button">
-//             Log in
-//           </Button>
-//           Or <a href="">register now!</a>
-//         </FormItem>
-//       </Form>
-//     );
-//   }
-// }
 
 const MyForm = Form.create()(NormalLoginForm);
 // const MyForm = Form.create()(FormContent);
